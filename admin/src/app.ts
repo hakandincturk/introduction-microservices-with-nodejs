e604ee5083ac201eb0cf79ea533aba9e088019a0
Hakan DİNÇTÜRK <58170470+SoFY111@users.noreply.github.com>
2022-06-29 13:31:29 +0300

    first commit

---
+++ admin/src/app.ts
@@ -0,0 +1,91 @@
+//C:\Users\hakan\OneDrive\Desktop\code\js\nodeJs\microservices\ex1\admin
+
+import * as express from 'express'
+import { Request, Response } from 'express';
+import * as cors from 'cors'
+import { createConnection } from 'typeorm'
+import { Product } from './entity/Product';
+
+createConnection().then(db => {
+
+  const productRepository = db.getRepository(Product);
+
+  const app = express()
+
+  app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200',]
+  }))
+
+  app.use(express.json())
+
+  app.get('/api/products', async (req: Request, res: Response) => {
+    try {
+      const products = await productRepository.find();
+      res.json({ type: true, data: products });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  });
+
+  app.post('/api/products', async (req: Request, res: Response) => {
+    try {
+      const product = await productRepository.create({
+        title: req.body.title,
+        image: req.body.image
+      });
+
+      const result = await productRepository.save(product);
+      res.json({ type: true, data: result });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  });
+
+  app.get('/api/products/:id', async (req: Request, res: Response) => {
+    try {
+      const product = await productRepository.findOne({ where: { id: parseInt(req.params.id, 10) } });
+
+      res.json({ type: true, data: product });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  })
+
+  app.put('/api/products/:id', async (req: Request, res: Response) => {
+    try {
+      const product = await productRepository.findOne({ where: { id: parseInt(req.params.id, 10) } });
+      productRepository.merge(product, req.body)
+      const result = await productRepository.save(product);
+      res.json({ type: true, data: result });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  })
+
+  app.delete('/api/products/:id', async (req: Request, res: Response) => {
+
+    try {
+      const result = await productRepository.delete(req.params.id);
+      res.json({ type: true, data: result });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  })
+
+  app.post('/api/products/:id/like', async (req: Request, res: Response) => {
+    try {
+      const product = await productRepository.findOne({ where: { id: parseInt(req.params.id, 10) } });
+      product.likes++;
+      const result = await productRepository.save(product);
+      res.json({ type: true, data: result });
+    } catch (error) {
+      res.json({ type: false, data: error.message });
+    }
+  })
+
+
+  app.listen(8000, () => {
+    console.info("SERVER LISTENING ON PORT 8000")
+  })
+
+});
